6721ed7aa7eb1639325a7ab2350c8887d8291369
Elena Lashkova <Elena.Lashkova@veritec.com.au>
2021-02-14 23:42:05 +1100

    name fadein

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var Index = function() {
 
-    var navBarShow = function(){
-                $("#navbar").hide().delay(2000).fadeIn(1000); 
+    var startFadeIn = function(){
+                $("#name").hide().delay(2000).fadeIn(1000); 
+                $("#profession").hide().delay(3000).fadeIn(1000); 
+                $("#navbar").hide().delay(4000).fadeIn(1000); 
             }    
 
 
     var init = function(){
-        navBarShow();
+        startFadeIn();
     };
 
     // Public API
